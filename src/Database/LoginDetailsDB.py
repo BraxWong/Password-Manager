d18910d3b54413358aefbf4346800967ec722db7
Brax Wong <83001805+BraxWong@users.noreply.github.com>
2024-09-26 19:25:05 +0800

    feature: implemented adding and fetching all entries from database functionalities.

--- src/Database/LoginDetailsDB.py
+++ src/Database/LoginDetailsDB.py
@@ -1,5 +1,29 @@
 import sqlite3
+import os
 
 class LoginDetailsDB:
     def __init__(self):
-        con=sqlite3.connect("Database/SQLFiles/LoginDetails.db")
+        self.PATHTOSQLDIR='Database/SQLFiles'
+        if not os.path.isdir(self.PATHTOSQLDIR):
+            os.makedirs(self.PATHTOSQLDIR)
+        con=sqlite3.connect(self.PATHTOSQLDIR+'/LoginDetails.db')
+        #Creates a cursor to the database
+        self.cur=con.cursor()
+        self.createLoginDetailsTable()
+
+    def createLoginDetailsTable(self):
+        self.cur.execute(
+            "CREATE TABLE if not exists login_details(website_name, password)"
+        )
+
+    def addEntryToDB(self,websiteName,password):
+        self.cur.execute(
+            f'INSERT INTO login_details VALUES ("{websiteName}", "{password}")'
+        )
+
+    def fetchAllFromDB(self):
+        self.cur.execute(
+            "SELECT * FROM login_details"
+        )
+        print(self.cur.fetchall())
+
