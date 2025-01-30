7c8449c09bb849e910d5bebf645a2762b09520ce
Brax Wong <ghostenjob@gmail.com>
2025-01-30 15:49:53 +0000

    feature: check the current url and html source to find out if it is in the login page and in need to provide website username and password.

--- src/WebsiteMonitor.py
+++ src/WebsiteMonitor.py
@@ -2,22 +2,31 @@ from selenium import webdriver
 from Database.LoginDetailsDB import LoginDetailsDB
 from WebsiteMonitorPopUp import *
 import time
+import requests
+
 class WebsiteMonitor:
     def __init__(self):
         self.popup = WebsiteMonitorPopup.instance()
+        self.currentURL = ""
 
     def run(self): 
-        try:
-            driver = webdriver.Chrome()
-            while True:
-                time.sleep(5)
-                self.openPopup()
-        except Exception as e:
-            print(f"Monitoring error: {e}")
-        finally:
-            if 'driver' in locals():
-                driver.quit()
+        driver = webdriver.Chrome()
+        while True:
+            time.sleep(5)
+            self.checkCurrentURL(driver.current_url)
 
+    def checkCurrentURL(self, URL):
+        if self.currentURL != URL and "login" in URL.lower():
+            self.currentURL = URL
+            loginDB = LoginDetailsDB()
+            details = loginDB.fetchAllFromDB()
+            r = requests.get(URL)
+            for detail in details:
+                if detail[0].lower() in URL.lower() or detail[0].lower() in r.text.lower():
+                    self.openPopup()
+        else:
+            print("SKIPPED checkCurrentURL()")
+            
     #DANGER: NOT WORKING YET AS THE PROGRAM IS UNABLE TO CREATE A POPUP OUTSIDE OF KIVY UI THREAD
     def openPopup(self):
         self.popup.showPopup("TESTING TITLE","TESTING CONTENT")
