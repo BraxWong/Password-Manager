4d7c9812739a400787190f9452cfc35e4429d6e2
Brax Wong <ghostenjob@gmail.com>
2025-02-26 18:29:06 +0000

    tests: completed checkPasswordInDB tests.

--- src/tests/UtilTests.py
+++ src/tests/UtilTests.py
@@ -1,11 +1,13 @@
+from sys import exception
 import unittest
 import Util.Util as util
-
+import Database.LoginDetailsDB as loginDetailsDB
 class UtilTestMethods(unittest.TestCase):
 
     def run_all_tests(self):
         self.test_dateComparison()
         self.test_generatePassword()
+        self.test_checkPasswordInDB()
 
 
 # ╭━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╮
@@ -69,4 +71,29 @@ class UtilTestMethods(unittest.TestCase):
             return (foundSymbol == True) and (validPasswordLength == True) 
         return validPasswordLength 
 
+    def test_checkPasswordInDB(self):
+        loginDetailsdb = loginDetailsDB.LoginDetailsDB()
+        try:
+            loginDetailsdb.addEntryToDB("TESTING1Website","TESTING1USERNAME","TESTINGPASSWORD")
+            self.assertEqual(True,util.checkPasswordInDB("TESTINGPASSWORD"))
+            loginDetailsdb.removeEntryFromDB("TESTING1Website")
+            print("test_checkPasswordInDB TEST 1: PASSED")
+        except AssertionError as e:
+            print(f"FAILED: {e}")
+
+        try:
+            loginDetailsdb.addEntryToDB("TESTING1Website","TESTING1USERNAME","TESTINGPASSWORD")
+            self.assertEqual(False,util.checkPasswordInDB("TESTINGPASSWORD1"))
+            loginDetailsdb.removeEntryFromDB("TESTING1Website")
+            print("test_checkPasswordInDB TEST 2: PASSED")
+        except AssertionError as e:
+            print(f"FAILED: {e}")
+
+        try:
+            loginDetailsdb.addEntryToDB("TESTING1Website","TESTING1USERNAME","TESTING2PASSWORD")
+            self.assertEqual(True,util.checkPasswordInDB("TESTING2PASSWORD"))
+            loginDetailsdb.removeEntryFromDB("TESTING1Website")
+            print("test_checkPasswordInDB TEST 3: PASSED")
+        except AssertionError as e:
+            print(f"FAILED: {e}")
 
