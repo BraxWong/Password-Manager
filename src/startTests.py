6b5b69ce6b44cb5f731ae6ab9c4f24879eec7473
BraxWong <ghostenjob@gmail.com>
2025-05-29 22:35:40 +0000

    feature: added a bash script to run the unit tests.

--- src/startTests.py
+++ src/startTests.py
@@ -2,12 +2,6 @@ import tests.UtilTests as utilTests
 import tests.passwordHashingTests as hashingTests
 import tests.wordSearchAlgorithmTests as wordSearchTests
 
-#     ╭━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╮
-#     ┃                                                                    ┃
-#     ┃ TODO: Create a CLI tool so users (Me) can decide what tests to run ┃
-#     ┃                                                                    ┃
-#     ╰━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╯
-
 while True:
     print("Here is a list of tests you can run:\nPress 1: Util Tests\nPress 2: Password Hashing Tests\nPress 3: Word Search Algorithm Tests\nPress 4: Run All Tests\nPress ANY key: Quit")
     userInput = input()
